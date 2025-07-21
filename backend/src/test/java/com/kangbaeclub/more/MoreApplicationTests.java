f1f10af54522ab4d569facc3277099cc2e4f1416
joonamin <joonamin44@gmail.com>
2025-07-21 17:59:42 +0900

    chore: local phase에 따른 환경설정 파일 분리

--- backend/src/test/java/com/kangbaeclub/more/MoreApplicationTests.java
+++ backend/src/test/java/com/kangbaeclub/more/MoreApplicationTests.java
@@ -2,8 +2,10 @@ package com.kangbaeclub.more;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MoreApplicationTests {
 
     @Test
