dab87749c3235949261eb3bc756804bf8a822f2a
Kang Minjun <97105309+joonamin@users.noreply.github.com>
2025-07-22 16:22:53 +0900

    chore: local phase에 따른 환경설정 파일 분리 (#12)
    * chore: local phase에 따른 환경설정 파일 분리
    
    * chore: 테스트 실행시에 'test' profile에서 테스트가 이루어질 수 있도록 수정합니다.
    
    * chore: setup mariadb in github actions

--- backend/src/test/java/com/kangbaeclub/more/filters/UserAuthenticationFilterTest.java
+++ backend/src/test/java/com/kangbaeclub/more/filters/UserAuthenticationFilterTest.java
@@ -15,6 +15,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -24,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+@ActiveProfiles("test")
 @SpringBootTest
 @AutoConfigureMockMvc
 class UserAuthenticationFilterTest {
