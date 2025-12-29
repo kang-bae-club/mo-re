2a2608796e64a04bcec6445bb80fad8000960bf3
sunyoung <tjsdud6677@gmail.com>
2025-12-29 23:01:26 +0900

    feat: App.tsx 로그인화면 route 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
+import { Routes, Route } from 'react-router-dom'
+import { HomePage } from './pages/home/ui/Page'
+import { RoomDetailPage } from './pages/room/RoomDetailPage'
 import { Login } from './components/Login'
 
-export const App = () => {
+const App = () => {
     return (
         <>
-            <Login />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/room/:roomId" element={<RoomDetailPage />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
         </>
     )
 }
+
+export default App
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
-import { App } from '@/App'
+import App from '@/App'
 
 async function enableMocking() {
     if (!import.meta.env.DEV) {
